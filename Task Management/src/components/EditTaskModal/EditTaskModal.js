743e5527a40a1711b3c4dd34f18ef034be964701
Satyam__Sharma <138864100+Satyams-23@users.noreply.github.com>
2023-11-30 14:36:32 +0530

    Update EditTaskModal.js

--- Task Management/src/components/EditTaskModal/EditTaskModal.js
+++ Task Management/src/components/EditTaskModal/EditTaskModal.js
@@ -1,85 +1,108 @@
 // components/EditTaskModal.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const EditTaskModal = ({ task, onEditTask, onClose }) => {
-    const [editedTask, setEditedTask] = useState({
-        name: '',
-        description: '',
-        priority: 'low',
+  const [editedTask, setEditedTask] = useState({
+    name: "",
+    description: "",
+    priority: "low",
+  });
+
+  useEffect(() => {
+    setEditedTask({
+      name: task.name || "",
+      description: task.description || "",
+      priority: task.priority || "low",
     });
+  }, [task]);
 
-    useEffect(() => {
-        setEditedTask({
-            name: task.name || '',
-            description: task.description || '',
-            priority: task.priority || 'low',
-        });
-    }, [task]);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
+    if (typeof onEditTask === "function") {
+      onEditTask({
+        ...task,
+        ...editedTask,
+      });
+    }
 
-        if (typeof onEditTask === 'function') {
-            onEditTask({
-                ...task,
-                ...editedTask,
-            });
-        }
+    onClose();
+  };
 
-        onClose();
-    };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,
+    }));
+  };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setEditedTask((prevTask) => ({
-            ...prevTask,
-            [name]: value,
-        }));
-    };
+  return (
+    <div className="modal">
+      <div className="modal-container bg-white p-5 rounded">
+        <hr></hr>
+        <h2>Edit Task</h2>
+        <hr></hr>
+        <form onSubmit={handleFormSubmit}>
+          <div className="mb-3">
+            <label>
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                value={editedTask.name}
+                onChange={handleInputChange}
+                placeholder=" Task Name:"
+              />
+            </label>
+          </div>
+          <div className="form-floating m-auto">
+            <textarea
+              class="form-control"
+              id="floatingTextarea"
+              value={editedTask.description}
+              onChange={handleInputChange}
+            ></textarea>
+            <label for="floatingTextarea">discriptions</label>
+          </div>
 
-
-    return (
-        <div className="modal">
-            <div className="modal-content">
-                <h2>Edit Task</h2>
-                <form onSubmit={handleFormSubmit}>
-                    <label>
-                        Task Name:
-                        <input
-                            type="text"
-                            name="name"
-                            value={editedTask.name}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Task Description:
-                        <textarea
-                            name="description"
-                            value={editedTask.description}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Priority:
-                        <select
-                            name="priority"
-                            value={editedTask.priority}
-                            onChange={handleInputChange}
-                        >
-                            <option value="low">Low</option>
-                            <option value="medium">Medium</option>
-                            <option value="high">High</option>
-                        </select>
-                    </label>
-                    <button type="submit">Save Changes</button>
-                    <button type="button" onClick={onClose}>
-                        Cancel
-                    </button>
-                </form>
+          <div className="row m-auto p-2">
+            <div className="col-auto m-auto">
+              <label
+                style={{
+                  fontFamily: "cursive",
+                  fontWeight: "bold",
+                  fontSize: "1.2rem",
+                }}
+              >
+                Priority :
+              </label>
             </div>
-        </div>
-    );
+            <div className="col">
+              <select
+                class="form-select"
+                placeholder="Priority"
+                value={editedTask.priority}
+                onChange={handleInputChange}
+              >
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <button className="btn btn-outline-primary p-2 m-2" type="submit">Save Changes</button>
+          </div>
+          <hr></hr>
+          <div>
+            <button className="btn btn-outline-dark p-2 m-2 mb-3" type="button" onClick={onClose}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default EditTaskModal;
