9015d0d26e932d19c17e09217a4c59685e50dfed
Satyam__Sharma <138864100+Satyams-23@users.noreply.github.com>
2023-11-30 14:35:53 +0530

    Update TaskList.js

--- Task Management/src/components/TaskList/TaskList.js
+++ Task Management/src/components/TaskList/TaskList.js
@@ -1,33 +1,44 @@
 // components/TaskList.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Task from './Task';
-import { saveTasksToLocalStorage } from '../../utils/localStorage'; // Import the saveTasksToLocalStorage function
+import React from "react";
+import { Link } from "react-router-dom";
+import Task from "./Task";
+import { saveTasksToLocalStorage } from "../../utils/localStorage"; // Import the saveTasksToLocalStorage function
 
 const TaskList = ({ tasks, onToggle, onDelete, onEditTask }) => {
-    const completedTasks = tasks.filter((task) => task.completed);
+  const completedTasks = tasks.filter((task) => task.completed);
 
+  return (
+    <div className="">
+      <hr></hr>
+      <h1 className="" style={{ fontFamily: "cursive", fontWeight: "bold" }}>
+        Task List
+      </h1>
+      <hr></hr>
+      <Link to="/add">
+        <button className="btn btn-outline-primary m-2">Add Task</button>
+      </Link>
 
-    return (
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          onEditTask={onEditTask}
+        />
+      ))}
+      {completedTasks.length > 0 && (
         <div>
-            <h2>Task List</h2>
-            <Link to="/add">Add Task</Link>
-
-            {tasks.map((task) => (
-                <Task key={task.id} task={task} onToggle={onToggle} onDelete={onDelete} onEditTask={onEditTask} />
-            ))}
-            {completedTasks.length > 0 && (
-                <div>
-                    <h3>Completed Tasks</h3>
-                    {completedTasks.map((task) => (
-                        <div key={task.id}>
-                            {task.name} - {task.priority}
-                        </div>
-                    ))}
-                </div>
-            )}
+          <h3>Completed Tasks</h3>
+          {completedTasks.map((task) => (
+            <div key={task.id}>
+              {task.name} - {task.priority}
+            </div>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default TaskList;
